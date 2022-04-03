5379d3c990e74b3f1fef2f7a6d48b96e663c9bc3
MisuruAraki <araki@ceed.jp>
2022-04-03 18:31:42 +0900

    #5 セッティング値の変更をわかりやすくする。

--- app/src/main/java/jp/ceed/kart/settings/ui/practice/viewModel/PracticeSettingItemViewModel.kt
+++ app/src/main/java/jp/ceed/kart/settings/ui/practice/viewModel/PracticeSettingItemViewModel.kt
@@ -1,8 +1,11 @@
 package jp.ceed.kart.settings.ui.practice.viewModel
 
+import android.content.Context
 import android.os.Parcelable
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import jp.ceed.kart.settings.R
 import kotlinx.parcelize.Parcelize
 
 
@@ -27,6 +30,20 @@ data class PracticeSettingItemViewModel(
         @Suppress("unchecked_cast")
         override fun <T : ViewModel> create(key: String, modelClass: Class<T>): T {
             return PracticeSettingItemViewModel(sessionId, fieldName, value, inputType, isChanged) as T
+        }
+    }
+
+    fun textColor(context: Context): Int{
+        return when {
+            isEditable -> {
+                ContextCompat.getColor(context, R.color.text_edit_text_enabled)
+            }
+            isChanged -> {
+                ContextCompat.getColor(context, R.color.text_setting_value_changed)
+            }
+            else -> {
+                ContextCompat.getColor(context, R.color.text_default)
+            }
         }
     }
 }
